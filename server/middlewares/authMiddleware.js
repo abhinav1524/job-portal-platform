65fbbf7396099d169f865ce18c6e11e0136a3dd6
abhinav1524 <83525313+abhinav1524@users.noreply.github.com>
2026-02-21 21:58:38 +0530

    authentication working
    build the authentication system in this now user can register and login successfully.

---
+++ server/middlewares/authMiddleware.js
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User";
+
+export const protect = async(req,res,next)=>{
+    let token;
+    if(
+        req.headers.authorization && 
+        req.headers.authorization.startWith("Bearer")
+    ){
+        try {
+            token = req.headers.authorization.split(" ")[1];
+            const decode = jwt.verify(token,process.env.JWT_SECRET);
+            req.user= await User.findById(decode.id).select("password");
+            next();
+        } catch (error) {
+            return res.status(500).json({message:"Not authorized, token failed"})
+        }
+    }
+    if(!token){
+        return res.status(401).json({message:"Not authorized, no token"});
+    }
+}
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
     import cors from "cors";
     import dotenv from "dotenv";
     import connectDB from "./config/db.js";
+    import authRoutes from "./routes/authRoutes.js";
 
     dotenv.config();
 
@@ -11,6 +12,9 @@
     app.use(cors());
     app.use(express.json());
 
+    console.log("Registering /api/auth routes");
+    //api route //
+    app.use("/api/auth",authRoutes);
     // test route /
     app.get('api/health',(req,res)=>{
         res.status(200).json({message: "server is running "})
