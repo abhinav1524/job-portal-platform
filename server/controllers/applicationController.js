8a89ce511eacbec9c66847b3765c3baeef920a5e
abhinav1524 <83525313+abhinav1524@users.noreply.github.com>
2026-02-24 21:45:02 +0530

    job applicants and update status done
    job applicants and update status done now the recruiter can see the applicants who applied for the job and update the status of the job if he selected a person for that job and the user get notified

--- server/controllers/applicationController.js
+++ server/controllers/applicationController.js
@@ -24,3 +24,30 @@ export const applyJob = async(req,res)=>{
         res.status(500).json({message:error.message})
     }
 }
+
+export const getJobApplicants = async(req,res)=>{
+    try {
+        const jobId=req.params.jobId;
+        const applications = await Application.find({job:jobId})
+        .populate("applicant","name email")
+        .populate("job","title company");
+        res.status(200).json(applications);
+    } catch (error) {
+        res.status(200).json({message:error.message});
+    }
+}
+
+export const updateApplicationStatus = async(req,res)=>{
+    try {
+        const {status} =req.body;
+        const application = await Application.findById(req.params.id);
+        if(!application){
+            return res.status(404).json({message:"Application not found"})
+        }
+        application.status=status;
+        await application.save();
+        res.status(200).json(application);
+    } catch (error) {
+        res.status(500).json({message:error.message});
+    }
+}
--- server/routes/applicationRoutes.js
+++ server/routes/applicationRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { applyJob } from "../controllers/applicationController.js";
+import { applyJob, getJobApplicants, updateApplicationStatus } from "../controllers/applicationController.js";
 import { protect, authorize } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/:id/apply", protect, authorize("seeker"), applyJob);
+router.get("/job/:jobId",protect,authorize("recruiter"),getJobApplicants);
+router.put("/:id/status",protect,authorize("recruiter"),updateApplicationStatus);
 
 export default router;
