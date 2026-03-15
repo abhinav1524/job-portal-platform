11e853ced650e210fcc570cfedbba06cab5ddcf3
abhinav1524 <83525313+abhinav1524@users.noreply.github.com>
2026-03-15 13:55:15 +0530

    hardcode the email to send reset password
    hardcode the email to send reset password

--- server/utils/sendEmails.js
+++ server/utils/sendEmails.js
@@ -5,7 +5,8 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const sendEmail = async (options) => {
   const { data, error } = await resend.emails.send({
     from: "onboarding@resend.dev",
-    to: options.email,
+    /*to: options.email,*/
+    to:"abhinavsaini1242@gmail.com",
     subject: options.subject,
     text: options.message,
   });
