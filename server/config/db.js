4a5061f203b6d98bc5b63a6c39638a16fe534afe
abhinav1524 <83525313+abhinav1524@users.noreply.github.com>
2026-03-15 12:26:05 +0530

    correct the name missmatch thing
    correct the name missmatch thing

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async ()=>{
     try {
-        const conn = await mongoose.connect(process.env.Mongo_URI);
+        const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDB connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`DB Connection Error : ${error.message}`);
