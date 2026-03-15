e59993bf3ef7127daf931d22235db11e8472d23d
abhinav1524 <83525313+abhinav1524@users.noreply.github.com>
2026-03-15 13:34:31 +0530

    change the email service
    change the email service from nodemailer to resender.

--- server/utils/sendEmails.js
+++ server/utils/sendEmails.js
@@ -1,25 +1,14 @@
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async (options) => {
-
-  const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",  // ← changed
-    port: 587,               // ← changed
-    secure: false, 
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS
-    }
-  });
-
-  const mailOptions = {
-    from: process.env.EMAIL_USER,
+  await resend.emails.send({
+    from: "onboarding@resend.dev", // use this until you add your own domain
     to: options.email,
     subject: options.subject,
-    text: options.message
-  };
-
-  await transporter.sendMail(mailOptions);
+    text: options.message,
+  });
 };
 
 export default sendEmail;
